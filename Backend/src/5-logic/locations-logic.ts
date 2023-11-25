62b76beb6fe1b6ed8eac051ed7439b22f8e4bb2e
izhar yefet <izhar.yefet@gmail.com>
2023-11-25 04:24:44 +0200

    Fixed delete information when closing browser

--- Backend/src/5-logic/locations-logic.ts
+++ Backend/src/5-logic/locations-logic.ts
@@ -55,82 +55,73 @@ async function addLocation(location:LocationModel): Promise<LocationModel> {
 }
 
 
-async function updateFullLocation(location:LocationModel):Promise<LocationModel> {
-    
+async function updateFullLocation(location:LocationModel):Promise<LocationModel> { 
     const errors = location.validatePut();
     if(errors) {
-            throw new ValidationError(errors);
-        }
-        
+       throw new ValidationError(errors);
+    }; 
+
     if (location.image){
         const sql =`SELECT locationId, imageName FROM locations WHERE locationId = ?`
         const imageInfo = await dal.execute(sql,[location.locationId]);
         const TakeTheName = imageInfo.shift()
         const imageName = TakeTheName.imageName
-          try {
-            await unlink(`./src/1-assets/images/`+imageName);
-          } catch (error) {
-            console.error('there was an error:', error.message);
-          }
-     const dotIndex = location.image.name.lastIndexOf(".");
-     const extension = location.image.name.substring(dotIndex);
-     location.imageName = uuid() + extension;
-     await location.image.mv("./src/1-assets/images/" + location.imageName);
-     delete location.image;
-    }
-const newStartDate = dayjs(location.locationsStartDate).format('YYYY-MM-DD HH:hh');
-const newEndDate = dayjs(location.locationsStartDate).format('YYYY-MM-DD HH:hh');
+        try {
+          await unlink(`./src/1-assets/images/`+imageName);
+        }
+        catch (error) {
+          console.error('there was an error:', error.message);
+        }
+        const dotIndex = location.image.name.lastIndexOf(".");
+        const extension = location.image.name.substring(dotIndex);
+        location.imageName = uuid() + extension;
+        await location.image.mv("./src/1-assets/images/" + location.imageName);
+        delete location.image;
+    };
+
+    const newStartDate = dayjs(location.locationsStartDate).format('YYYY-MM-DD HH:hh');
+    const newEndDate = dayjs(location.locationsStartDate).format('YYYY-MM-DD HH:hh');
     const sql = `UPDATE locations SET locationName =?, price = ?,locationsInfo = ?, locationsStartDate = ?,locationsEndDate = ?,imageName = ? WHERE locationId = ?`;
-            const result: OkPacket = await dal.execute(sql,[location.locationName,location.price,location.locationsInfo,newStartDate,newEndDate,location.imageName,location.locationId]);
-              if(result.affectedRows === 0 ){
-             throw new ResourceNotFoundError(location.locationId);
-    }
+    const result: OkPacket = await dal.execute(sql,[location.locationName,location.price,location.locationsInfo,newStartDate,newEndDate,location.imageName,location.locationId]);
+  
+    if(result.affectedRows === 0 ){
+       throw new ResourceNotFoundError(location.locationId);
+    };
     socketLogic.reportUpdateLocation(location)
     return location;
 
-}
+};
 
 async function updatePartialLocation(location:LocationModel):Promise<LocationModel> {
-    // const errors = location.validatePatch();
-    // if(errors){
-    //     throw new ValidationError(errors);
-    // }
     const dbLocations = await getDetailsLocation(location.locationId);
     const dbLocation= dbLocations[0];
-
     for(const prop in location){
-        if(location[prop] !== undefined){
-            dbLocation[prop] = location[prop];
-        }
-    }
+      if(location[prop] !== undefined){
+        dbLocation[prop] = location[prop];
+       };
+    };
     const updatedLocation = await updateFullLocation(dbLocation);
     socketLogic.reportUpdateLocation(location)
     return updatedLocation;
-}
+};
 
 
 async function deleteLocation(locationId:number):Promise<void> {
-
-const sql =`SELECT locationId, imageName FROM locations WHERE locationId = ?`
-const imageInfo = await dal.execute(sql,[locationId]);
-const TakeTheName = imageInfo.shift()
-const imageName = TakeTheName.imageName
-const StringImageName = JSON.stringify(imageInfo)
-console.log(`imageName ${imageInfo}`);
-console.log(`successfully deleted`+StringImageName);
-console.log(StringImageName);
-
-  try {
-    await unlink(`./src/1-assets/images/`+imageName);
-  } catch (error) {
-    console.error('there was an error:', error.message);
-  }
-
+  
+    const sql =`SELECT locationId, imageName FROM locations WHERE locationId = ?`
+    const imageInfo = await dal.execute(sql,[locationId]);
+    const TakeTheName = imageInfo.shift()
+    const imageName = TakeTheName.imageName
+    
+    try { await unlink(`./src/1-assets/images/`+imageName)}
+    catch (error) { console.error('there was an error:', error.message);};
+   
     const sqlForDelete = `DELETE FROM locations WHERE locationId = ?`;
     const result = await dal.execute(sqlForDelete,[locationId]);
+   
     if(result.affectedRows === 0 ){
         throw new ResourceNotFoundError(locationId)
-    }
+    };
     socketLogic.reportDeleteLocation(locationId)
 }
 
--- frontend/src/Components/LayoutArea/Layout/Layout.tsx
+++ frontend/src/Components/LayoutArea/Layout/Layout.tsx
@@ -8,10 +8,26 @@ import RoutingForUnknownUsers from "../Routing/RoutingForUnknownUsers";
 import "./Layout.css";
 import Role from "../../../Models/RoleModel";
 import RoutingForUser from "../Routing/RoutingForUser";
+import authService from "../../../Services/AuthService";
+import socketService from "../../../Services/SocketService";
+import locationsService from "../../../Services/LocationsService";
+import userService from "../../../Services/UserService";
+import followerService from "../../../Services/FollowerService";
 
 function Layout(): JSX.Element {
     const [role, setRole] = useState<boolean>(false);
     const [user, setUser] = useState<UserModel>();
+
+ 
+    window.onunload = () => {
+        window.localStorage.clear()
+        authService.logout();
+            socketService.disconnect();
+            locationsService.logout();
+            followerService.logout();
+            userService.logout();
+        console.log("Layout");
+     }
 
 
         useEffect(()=>{
